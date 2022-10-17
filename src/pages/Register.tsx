14bb6ca53adc6d90942e5e57ee2f2ac69c12606e
Justin Cletus <justin.cletus@pantheon.io>
2022-10-17 05:37:16 +0530

    initial commit

---
+++ src/pages/Register.tsx
@@ -0,0 +1,47 @@
+import React, { SyntheticEvent, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false)
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        await fetch("http://localhost:8000/api/register", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+
+        setRedirect(true)
+
+        // const content = await response.json();
+        // console.log(content)
+
+    }
+
+    if (redirect) {
+        return <Navigate to="/login/"/>;
+    }
+
+    return (
+        <form onSubmit={submit}>
+          <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+          <input type="test" className="form-control" placeholder="Name" required 
+          onChange={e => setName(e.target.value)} />
+            <input type="email" className="form-control" placeholder="name@example.com" required 
+            onChange={e => setEmail(e.target.value)} />
+            <input type="password" className="form-control" placeholder="Password" required 
+            onChange={e => setPassword(e.target.value)} />      
+          <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Register
